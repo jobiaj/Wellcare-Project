d06dd2faa382f085bd3d436e80684a4a07a6ce3a
soumya62 <pattnaiks62@gmail.com>
2018-07-28 18:18:32 +0530

    models

--- wellcare/company/admin.py
+++ wellcare/company/admin.py
@@ -2,7 +2,19 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-from models import WellCareUser, EmployeeInfo
+from models import WellCareUser, EmployeeInfo,ISP, Representative, ServiceType, Daily_routine, General, Medicle, PersonalCare, Mobility, Equipment_and_aids, Behaviour, Household_tasks, Transport
 admin.site.register(WellCareUser)
 admin.site.register(EmployeeInfo)
+admin.site.register(ISP)
+admin.site.register(Representative)
+admin.site.register(ServiceType)
+admin.site.register(Daily_routine)
+admin.site.register(General)
+admin.site.register(Medicle)
+admin.site.register(PersonalCare)
+admin.site.register(Mobility)
+admin.site.register(Equipment_and_aids)
+admin.site.register(Behaviour)
+admin.site.register(Household_tasks)
+admin.site.register(Transport)
 
